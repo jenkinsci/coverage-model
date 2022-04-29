a5d7f6ea9cc67809b786978abf07b2888f2fdc7a
Melissa Bauer <bauer.melissa@hm.edu>
2022-04-29 15:46:54 +0200

    Add parser for Jacoco reports

---
+++ src/main/java/edu/hm/hafner/parser/JacocoParser.java
@@ -0,0 +1,226 @@
+package edu.hm.hafner.parser;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+
+import edu.hm.hafner.coverage.Coverage;
+import edu.hm.hafner.coverage.CoverageLeaf;
+import edu.hm.hafner.coverage.CoverageMetric;
+import edu.hm.hafner.coverage.CoverageNode;
+import edu.hm.hafner.coverage.FileCoverageNode;
+import edu.hm.hafner.coverage.PackageCoverageNode;
+
+/**
+ * A parser which parses reports made by Jacoco into a Java Object Model.
+ *
+ * @author Melissa Bauer
+ */
+public class JacocoParser {
+
+    /** Attributes of the XML elements. */
+    private static final QName NAME = new QName("name");
+    private static final QName SOURCEFILENAME = new QName("sourcefilename");
+    private static final QName TYPE = new QName("type");
+    private static final QName MISSED = new QName("missed");
+    private static final QName COVERED = new QName("covered");
+
+    private static CoverageNode rootNode;
+    private static CoverageNode currentPackageNode;
+    private static CoverageNode currentNode;
+    private static String filename;
+    private static HashMap<String, ArrayList<CoverageNode>> classNodesMap = new HashMap<>();
+
+    public CoverageNode getRootNode() {
+        return rootNode;
+    }
+
+    /**
+     * Creates a new JacocoParser which parses the given Jacoco xml report into a java data model.
+     *
+     * @param path path to report file
+     */
+    public JacocoParser(final String path) {
+        parseFile(path);
+    }
+
+    /**
+     * Parses xml report at given path.
+     *
+     * @param path path to report file
+     */
+    private static void parseFile(final String path) {
+        XMLInputFactory factory = XMLInputFactory.newInstance();
+        System.out.println("FACTORY: " + factory);
+
+        XMLEventReader r;
+        try {
+            r = factory.createXMLEventReader(path, new FileInputStream(path));
+            while (r.hasNext()) {
+                XMLEvent e = r.nextEvent();
+
+                if (e.isStartDocument()) {
+                    String systemId = e.getLocation().getSystemId();
+                    String[] parts = systemId.split("/");
+                    filename = parts[parts.length - 1];
+                }
+
+                if (e.isStartElement()) {
+                    startElement(e.asStartElement());
+                }
+
+                if (e.isEndElement()) {
+                    endElement(e.asEndElement().getName());
+                }
+            }
+        }
+        catch (XMLStreamException | FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Creates a node or a leaf depending on the given element type.
+     *
+     * @param element the complete tag element including attributes
+     */
+    private static void startElement(final StartElement element) {
+        String name = element.getName().toString();
+
+        switch (name) {
+            case "report":
+                // module name consists of name attribute of the element together with the filename, divided with :
+                String moduleName = element.getAttributeByName(NAME).getValue() + ": " + filename;
+
+                rootNode = new CoverageNode(CoverageMetric.MODULE, moduleName);
+                currentNode = rootNode;
+                break;
+
+            case "package":
+                String packageName = element.getAttributeByName(NAME).getValue();
+                CoverageNode packageNode = new PackageCoverageNode(packageName.replaceAll("/", "."));
+                rootNode.add(packageNode);
+
+                currentPackageNode = packageNode; // save for later to be able to add fileNodes
+                currentNode = packageNode;
+                break;
+
+            case "class":
+                handleClassElement(element);
+                break;
+
+            case "sourcefile":
+                String fileName = element.getAttributeByName(NAME).getValue();
+                CoverageNode fileNode = new FileCoverageNode(fileName);
+
+                // add all classNodes to current fileNode
+                ArrayList<CoverageNode> classNodeList = classNodesMap.get(fileName);
+                for (CoverageNode classNode : classNodeList) {
+                    fileNode.add(classNode);
+                }
+
+                currentPackageNode.add(fileNode);
+                break;
+
+            case "method":
+                CoverageNode methodNode = new CoverageNode(CoverageMetric.METHOD,
+                        element.getAttributeByName(NAME).getValue());
+
+                currentNode.add(methodNode);
+                currentNode = methodNode;
+                break;
+
+            case "counter":
+                handleCounterElement(element);
+                break;
+        }
+    }
+
+    /**
+     * Creates a class node and saves it to a map.
+     * This is necessary because classes occur before sourcefiles in the report.
+     * But in the java model, classes are children of files.
+     *
+     * @param element the current report element
+     */
+    private static void handleClassElement(final StartElement element) {
+        CoverageNode classNode = new CoverageNode(CoverageMetric.CLASS, element.getAttributeByName(NAME).getValue());
+
+        String sourcefileName = element.getAttributeByName(SOURCEFILENAME).getValue();
+
+        // Adds current class node as part of a list to the map
+        ArrayList<CoverageNode> classNodesList;
+        if (classNodesMap.containsKey(sourcefileName)) {
+            classNodesList = classNodesMap.get(sourcefileName);
+        }
+        else {
+            classNodesList = new ArrayList<>();
+        }
+        classNodesList.add(classNode);
+        classNodesMap.put(sourcefileName, classNodesList);
+
+        currentNode = classNode;
+    }
+
+    /**
+     * Creates a leaf with general covered/missed information under a method-node.
+     *
+     * @param element the current report element
+     */
+    private static void handleCounterElement(final StartElement element) {
+        String currentType = element.getAttributeByName(TYPE).getValue();
+
+        // We only look for data on method layer
+        if (currentNode.getMetric() != CoverageMetric.METHOD) {
+            return;
+        }
+
+        CoverageMetric coverageMetric;
+        switch (currentType) {
+            case "LINE":
+                coverageMetric = CoverageMetric.LINE;
+                break;
+            case "INSTRUCTION":
+                coverageMetric = CoverageMetric.INSTRUCTION;
+                break;
+            case "BRANCH":
+                coverageMetric = CoverageMetric.BRANCH;
+                break;
+            default:
+                return;
+        }
+
+        CoverageLeaf coverageLeaf = new CoverageLeaf(coverageMetric,
+                new Coverage(Integer.parseInt(element.getAttributeByName(COVERED).getValue()),
+                        Integer.parseInt(element.getAttributeByName(MISSED).getValue())));
+
+        currentNode.add(coverageLeaf);
+    }
+
+    /**
+     * Depending on the tag, either resets the map containing the class objects
+     * or sets the current node back to the class node.
+     *
+     * @param qName name of the ending tag
+     */
+    private static void endElement(final QName qName) {
+        switch (qName.toString()) {
+            case "package":
+                currentNode = rootNode;
+                currentPackageNode = null;
+                classNodesMap = new HashMap<>();
+                break;
+
+            case "method":
+                currentNode = currentNode.getParent();
+                break;
+        }
+    }
+}
---
+++ src/test/java/edu/hm/hafner/parser/JacocoParserTest.java
@@ -0,0 +1,187 @@
+package edu.hm.hafner.parser;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.commons.lang3.math.Fraction;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import edu.hm.hafner.coverage.Coverage;
+import edu.hm.hafner.coverage.CoverageMetric;
+import edu.hm.hafner.coverage.CoverageNode;
+
+import static edu.hm.hafner.coverage.CoverageMetric.*;
+import static edu.hm.hafner.coverage.CoverageMetric.CLASS;
+import static edu.hm.hafner.coverage.CoverageMetric.FILE;
+import static edu.hm.hafner.coverage.assertions.Assertions.*;
+
+/**
+ * Tests the class {@link JacocoParser}.
+ *
+ * @author Ullrich Hafner
+ */
+class JacocoParserTest {
+    private static final String PROJECT_NAME = "Java coding style: jacoco-codingstyle.xml";
+
+    @BeforeAll
+    static void beforeAll() {
+        Locale.setDefault(Locale.ENGLISH);
+    }
+
+    @Test
+    void shouldReturnEmptyCoverageIfNotFound() {
+        CoverageNode root = readExampleReport();
+
+        assertThat(root.getCoverage(CoverageMetric.valueOf("new"))).isNotSet();
+    }
+
+    @Test
+    void shouldConvertCodingStyleToTree() {
+        CoverageNode tree = readExampleReport();
+
+        verifyCoverageMetrics(tree);
+
+        assertThat(tree.getAll(MODULE)).hasSize(1);
+        assertThat(tree.getAll(PACKAGE)).hasSize(1);
+        List<CoverageNode> files = tree.getAll(FILE);
+        assertThat(files).hasSize(10);
+        assertThat(tree.getAll(CLASS)).hasSize(18);
+        assertThat(tree.getAll(METHOD)).hasSize(102);
+
+        assertThat(tree).hasOnlyMetrics(MODULE, PACKAGE, FILE, CLASS, METHOD, LINE, BRANCH, INSTRUCTION)
+                .hasToString("[Module] " + PROJECT_NAME);
+        assertThat(tree.getMetricsDistribution()).containsExactly(
+                entry(MODULE, new Coverage(1, 0)),
+                entry(PACKAGE, new Coverage(1, 0)),
+                entry(FILE, new Coverage(7, 3)),
+                entry(CLASS, new Coverage(15, 3)),
+                entry(METHOD, new Coverage(97, 5)),
+                entry(LINE, new Coverage(294, 29)),
+                entry(INSTRUCTION, new Coverage(1260, 90)),
+                entry(BRANCH, new Coverage(109, 7)));
+        assertThat(tree.getMetricsPercentages()).containsExactly(
+                entry(MODULE, Fraction.ONE),
+                entry(PACKAGE, Fraction.ONE),
+                entry(FILE, Fraction.getFraction(7, 7 + 3)),
+                entry(CLASS, Fraction.getFraction(15, 15 + 3)),
+                entry(METHOD, Fraction.getFraction(97, 97 + 5)),
+                entry(LINE, Fraction.getFraction(294, 294 + 29)),
+                entry(INSTRUCTION, Fraction.getFraction(1260, 1260 + 90)),
+                entry(BRANCH, Fraction.getFraction(109, 109 + 7)));
+
+        assertThat(tree.getChildren()).hasSize(1).element(0).satisfies(
+                packageNode -> assertThat(packageNode).hasName("edu.hm.hafner.util")
+        );
+    }
+
+    @Test
+    void shouldSplitPackages() {
+        CoverageNode tree = readExampleReport();
+
+        tree.splitPackages();
+
+        verifyCoverageMetrics(tree);
+
+        assertThat(tree.getAll(PACKAGE)).hasSize(4);
+        assertThat(tree.getMetricsDistribution()).contains(
+                entry(PACKAGE, new Coverage(4, 0)));
+
+        assertThat(tree.getChildren()).hasSize(1).element(0).satisfies(
+                packageNode -> assertThat(packageNode).hasName("edu")
+                        .hasParent()
+                        .hasParentName(PROJECT_NAME)
+        );
+    }
+
+    @Test
+    void shouldNotSplitPackagesIfOnWrongHierarchyNode() {
+        CoverageNode tree = readExampleReport();
+        CoverageNode packageNode = tree.getChildren().get(0);
+        assertThat(packageNode).hasName("edu.hm.hafner.util").hasPath("edu/hm/hafner/util");
+
+        List<CoverageNode> files = packageNode.getChildren();
+
+        packageNode.splitPackages();
+        assertThat(packageNode).hasName("edu.hm.hafner.util");
+        assertThat(packageNode).hasChildren(files);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenObtainingAllBasicBlocks() {
+        CoverageNode tree = readExampleReport();
+
+        assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> tree.getAll(LINE));
+        assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> tree.getAll(BRANCH));
+        assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> tree.getAll(INSTRUCTION));
+    }
+
+    @Test
+    void shouldCreatePackageName() {
+        CoverageNode tree = readExampleReport();
+
+        String fileName = "Ensure.java";
+        assertThat(tree.find(FILE, fileName)).isNotEmpty().hasValueSatisfying(
+                node -> assertThat(node).hasName(fileName)
+                        .hasParentName("edu.hm.hafner.util")
+                        .hasParent()
+                        .isNotRoot()
+        );
+
+        tree.splitPackages();
+        assertThat(tree.find(FILE, fileName)).isNotEmpty().hasValueSatisfying(
+                node -> assertThat(node).hasName(fileName)
+                        .hasParentName("edu.hm.hafner.util")
+                        .hasParent()
+                        .isNotRoot()
+        );
+    }
+
+    private void verifyCoverageMetrics(final CoverageNode tree) {
+        assertThat(tree.getCoverage(LINE)).isSet()
+                .hasCovered(294)
+                .hasCoveredPercentage(Fraction.getFraction(294, 294 + 29))
+                .hasMissed(29)
+                .hasMissedPercentage(Fraction.getFraction(29, 294 + 29))
+                .hasTotal(294 + 29);
+        assertThat(tree.printCoverageFor(LINE)).isEqualTo("91.02%");
+        assertThat(tree.printCoverageFor(LINE, Locale.GERMAN)).isEqualTo("91,02%");
+
+        assertThat(tree.getCoverage(BRANCH)).isSet()
+                .hasCovered(109)
+                .hasCoveredPercentage(Fraction.getFraction(109, 109 + 7))
+                .hasMissed(7)
+                .hasMissedPercentage(Fraction.getFraction(7, 109 + 7))
+                .hasTotal(109 + 7);
+        assertThat(tree.printCoverageFor(BRANCH)).isEqualTo("93.97%");
+        assertThat(tree.printCoverageFor(BRANCH, Locale.GERMAN)).isEqualTo("93,97%");
+
+        assertThat(tree.getCoverage(INSTRUCTION)).isSet()
+                .hasCovered(1260)
+                .hasCoveredPercentage(Fraction.getFraction(1260, 1260 + 90))
+                .hasMissed(90)
+                .hasMissedPercentage(Fraction.getFraction(90, 1260 + 90))
+                .hasTotal(1260 + 90);
+        assertThat(tree.printCoverageFor(INSTRUCTION)).isEqualTo("93.33%");
+        assertThat(tree.printCoverageFor(INSTRUCTION, Locale.GERMAN)).isEqualTo("93,33%");
+
+        assertThat(tree.getCoverage(MODULE)).isSet()
+                .hasCovered(1)
+                .hasCoveredPercentage(Fraction.ONE)
+                .hasMissed(0)
+                .hasMissedPercentage(Fraction.ZERO)
+                .hasTotal(1);
+        assertThat(tree.printCoverageFor(MODULE)).isEqualTo("100.00%");
+        assertThat(tree.printCoverageFor(MODULE, Locale.GERMAN)).isEqualTo("100,00%");
+
+        assertThat(tree).hasName(PROJECT_NAME)
+                .doesNotHaveParent()
+                .isRoot()
+                .hasMetric(MODULE).hasParentName("^");
+    }
+
+    private CoverageNode readExampleReport() {
+        JacocoParser parser = new JacocoParser("src/test/resources/jacoco-codingstyle.xml");
+        return parser.getRootNode();
+    }
+}
