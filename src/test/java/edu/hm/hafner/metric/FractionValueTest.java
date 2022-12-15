55ed79e26a2ceff62c55642c1aa77fa9eaacfc08
Matthias Baier <mbaier@hm.edu>
2022-12-15 11:20:52 +0100

    updated FractionValueTest

--- src/test/java/edu/hm/hafner/metric/FractionValueTest.java
+++ src/test/java/edu/hm/hafner/metric/FractionValueTest.java
@@ -3,6 +3,8 @@ package edu.hm.hafner.metric;
 import org.apache.commons.lang3.math.Fraction;
 import org.junit.jupiter.api.Test;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
+
 import static edu.hm.hafner.metric.assertions.Assertions.*;
 
 /**
@@ -21,5 +23,66 @@ class FractionValueTest {
 
         assertThat(fifty.add(fifty)).isEqualTo(hundred);
         assertThat(fifty.max(hundred)).isEqualTo(hundred);
+        assertThat(hundred.max(fifty)).isEqualTo(hundred);
     }
+
+    @Test
+    void shouldGetFraction() {
+        FractionValue testFractionValue = new FractionValue(Metric.LINE, Fraction.getFraction(50, 1));
+        assertThat(testFractionValue.getFraction()).isEqualTo(Fraction.getFraction(50, 1));
+    }
+
+    @Test
+    void shouldThrowExceptionAdd() {
+        var fifty = new FractionValue(Metric.LINE, Fraction.getFraction(50, 1));
+        var hundred = new FractionValue(Metric.FILE, Fraction.getFraction(100, 1));
+
+        var fiftyLoc = new FractionValue(Metric.LOC, Fraction.getFraction(50, 1));
+        var loc = new LinesOfCode(2);
+
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> fifty.add(hundred));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> fiftyLoc.add(loc));
+    }
+
+
+    @Test
+    void shouldReturnDelta() {
+        var fifty = new FractionValue(Metric.LINE, Fraction.getFraction(50, 1));
+        var hundred = new FractionValue(Metric.LINE, Fraction.getFraction(100, 1));
+
+        assertThat(fifty.isBelowThreshold(50.1)).isTrue();
+        assertThat(fifty.isBelowThreshold(50)).isFalse();
+
+        assertThat(hundred.delta(fifty)).isEqualTo(Fraction.getFraction(50, 1));
+    }
+    @Test
+    void shouldThrowExceptionDelta() {
+        var fifty = new FractionValue(Metric.LINE, Fraction.getFraction(50, 1));
+        var hundred = new FractionValue(Metric.FILE, Fraction.getFraction(100, 1));
+
+        var fiftyLoc = new FractionValue(Metric.LOC, Fraction.getFraction(50, 1));
+        var loc = new LinesOfCode(2);
+
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> fifty.delta(hundred));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> fiftyLoc.delta(loc));
+    }
+
+    @Test
+    void shouldThrowExceptionMax() {
+        var fifty = new FractionValue(Metric.LINE, Fraction.getFraction(50, 1));
+        var hundred = new FractionValue(Metric.FILE, Fraction.getFraction(100, 1));
+
+        var fiftyLoc = new FractionValue(Metric.LOC, Fraction.getFraction(50, 1));
+        var loc = new LinesOfCode(2);
+
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> fifty.max(hundred));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> fiftyLoc.max(loc));
+    }
+
+    @Test
+    void equalTest() {
+        EqualsVerifier.forClass(FractionValue.class).withRedefinedSuperclass().verify();
+    }
+
+
 }
