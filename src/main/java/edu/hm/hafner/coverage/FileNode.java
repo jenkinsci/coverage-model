827f9443f5e7f4d2f7ef4d212231fc36c7c153b1
Ulli Hafner <ullrich.hafner@gmail.com>
2023-04-11 11:03:28 +0200

    Make `readResolve` protected.

--- src/main/java/edu/hm/hafner/coverage/FileNode.java
+++ src/main/java/edu/hm/hafner/coverage/FileNode.java
@@ -73,7 +73,7 @@ public final class FileNode extends Node {
      *
      * @return this
      */
-    private Object readResolve() {
+    protected Object readResolve() {
         if (relativePath == null) {
             relativePath = TreeString.valueOf(StringUtils.EMPTY);
         }
