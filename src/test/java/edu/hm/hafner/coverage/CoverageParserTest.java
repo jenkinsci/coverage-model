94095e590f4d8be27977633c3b64bc710aadffa7
Ulli Hafner <ullrich.hafner@gmail.com>
2023-09-21 17:28:08 +0200

    Remove exception testing method.

--- src/test/java/edu/hm/hafner/coverage/CoverageParserTest.java
+++ src/test/java/edu/hm/hafner/coverage/CoverageParserTest.java
@@ -2,8 +2,6 @@ package edu.hm.hafner.coverage;
 
 import org.junit.jupiter.api.Test;
 
-import edu.hm.hafner.util.SecureXmlParserFactory.ParsingException;
-
 import static edu.hm.hafner.coverage.assertions.Assertions.*;
 
 /**
@@ -14,17 +12,8 @@ import static edu.hm.hafner.coverage.assertions.Assertions.*;
  */
 class CoverageParserTest {
     @Test
-    void shouldCreateEofException() {
-        var parsingException = CoverageParser.createEofException();
-
-        assertThat(parsingException)
-                .isInstanceOf(ParsingException.class)
-                .hasMessage("Unexpected end of file");
-    }
-
-    @Test
     void shouldReturnZeroOnInvalidStringParsing() {
-        assertThat(CoverageParser.parseInteger("NO_NUMBER"))
-                .isEqualTo(0);
+        assertThat(CoverageParser.parseInteger("NO_NUMBER")).isEqualTo(0);
+        assertThat(CoverageParser.parseInteger("111")).isEqualTo(111);
     }
 }
