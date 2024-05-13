18fc533140fec5bc6d14365f52de049c14cb2f37
Pierson Yieh <pierson_yieh@intuit.com>
2024-05-13 15:43:48 -0700

    Change Integer to primitive int

--- src/main/java/edu/hm/hafner/coverage/parser/CloverParser.java
+++ src/main/java/edu/hm/hafner/coverage/parser/CloverParser.java
@@ -4,6 +4,7 @@ import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import edu.hm.hafner.coverage.*;
 import edu.hm.hafner.util.FilteredLog;
 import edu.hm.hafner.util.SecureXmlParserFactory;
+import edu.hm.hafner.util.SecureXmlParserFactory.ParsingException;
 import edu.hm.hafner.util.TreeString;
 
 import javax.xml.namespace.QName;
@@ -14,8 +15,6 @@ import javax.xml.stream.events.XMLEvent;
 import java.io.Reader;
 
 public class CloverParser extends CoverageParser {
-    private static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(CloverParser.class.getName());
-
     private static final long serialVersionUID = -1903059983931698657L;
 
     private static final QName COVERAGE = new QName("coverage");
@@ -125,23 +124,23 @@ public class CloverParser extends CoverageParser {
             if (event.isStartElement()) {
                 var e = event.asStartElement();
                 if (METRICS.equals(e.getName())) {
-                    Integer condTotal = getIntegerValueOf(e, CONDITIONALS);
-                    Integer condCovered = getIntegerValueOf(e, COVERED_CONDITIONALS);
-                    Integer stmntsTotal = getIntegerValueOf(e, STATEMENTS);
-                    Integer stmntsCovered = getIntegerValueOf(e, COVERED_STATEMENTS);
+                    int condTotal = getIntegerValueOf(e, CONDITIONALS);
+                    int condCovered = getIntegerValueOf(e, COVERED_CONDITIONALS);
+                    int stmntsTotal = getIntegerValueOf(e, STATEMENTS);
+                    int stmntsCovered = getIntegerValueOf(e, COVERED_STATEMENTS);
                     classNode.addValue(createValue("CONDITIONAL", condCovered, condTotal - condCovered));
                     classNode.addValue(createValue("INSTRUCTION", stmntsCovered, stmntsTotal - stmntsCovered));
 
                 } else if (LINE.equals(e.getName())) {
-                    Integer line =  Integer.parseInt(getValueOf(e, NUM));
-                    Integer count = Integer.parseInt(getValueOf(e, COUNT));
+                    int line =  Integer.parseInt(getValueOf(e, NUM));
+                    int count = Integer.parseInt(getValueOf(e, COUNT));
                     if (count > 0) {
                         fileNode.addCounters(line, count, 0);
                     } else {
                         fileNode.addCounters(line, 0, 1);
                     }
                 } else {
-                    LOGGER.warning("Unexpected element in <file> block: " + e.getName());
+                    new ParsingException(String.format("Unexpected element '%s' in <file> block in file '%s'", e.getName(), fileName));
                 }
             } else if (event.isEndElement()) {
                 var endElement = event.asEndElement();
